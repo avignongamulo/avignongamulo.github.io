f050b625253cd467fb27182cfbca40a0306caaff
avignongamulo <stu.avignon.gamulo@ihsnola.org>
2022-02-23 03:55:04 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -2,6 +2,7 @@
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
   
+
 function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
@@ -10,14 +11,32 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+ var BOARD_WIDTH = $('#board').width();
+ var BOARD_HEIGHT = $('#board').height();
+ 
+
+
+
+  var ARROWKEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN" : 40
+  }
   
+
+  var positionX = 0;
+  var positionY = 0;
+  var speedX = 0;
+  var speedY = 0;
+
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keyup", handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,20 +46,88 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    // update position of the game item
+    repositionBox();
+    // check for collisions
+    checkForBorderCollisions();
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    // press up key ---> accelerate box in negative y
+    changeSpeedY(-5, event.which, ARROWKEY.UP)
+
+    
+    // press down key ---> accelerate box in positive y
+    changeSpeedY(5, event.which, ARROWKEY.DOWN)
+
+    
+    // press left key ---> accelerate box in negative x 
+    changeSpeedX(-5, event.which, ARROWKEY.LEFT)
+
+  
+    // press right key ---> accelerate box in positive x
+    changeSpeedX(5, event.which, ARROWKEY.RIGHT)
+
+    
+   
+
+  }
+
+  function handleKeyUp(event) {
+    
+    changeSpeedY(0, event.which, ARROWKEY.UP)
+   
+    changeSpeedY(0, event.which, ARROWKEY.DOWN)
+   
+    changeSpeedX(0, event.which, ARROWKEY.LEFT)
+    
+    changeSpeedX(0, event.which, ARROWKEY.RIGHT)
+   
 
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function repositionBox(){
+    positionX += speedX;
+    $("#walker").css("left", positionX);
+
+    positionY += speedY;
+    $("#walker").css("top", positionY);
+  }
+ 
+
+  function changeSpeedY(newSpeed, keycode, arrowKey){
+    if (keycode === arrowKey){
+      speedY = newSpeed;
+  }
+}
+
+  function changeSpeedX(newSpeed, keycode, arrowKey){
+    if (keycode === arrowKey){
+      speedX = newSpeed;
+  }
+}
+
+function checkForBorderCollisions(){
+  if (positionX > BOARD_WIDTH - $("#walker").width()){
+    positionX = BOARD_WIDTH - $("#walker").width();
+  }
+  else if (positionX < 0){
+   positionX = 0;
+  }
+  if (positionY > BOARD_HEIGHT - $("#walker").height()){
+    positionY = BOARD_HEIGHT - $("#walker").height()
+  }
+  else if (positionY < 0){
+   positionY = 0;
+  }
+}
 
   
   function endGame() {
@@ -50,5 +137,6 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+ 
   
 }
